611512be660d75910658c57ad6c7fb9eedd3f235
Tudubahindo <77055834+Tudubahindo@users.noreply.github.com>
2025-01-20 22:46:26 +0100

    Delete NSL_SIMULATOR_backup_20250103 directory

--- NSL_SIMULATOR_backup_20250103/simulator/l07.sh
+++
@@ -1,6 +0,0 @@
-time ./simulator.exe 1 ../l7/mc_solid_calibration/
-time ./simulator.exe 1 ../l7/mc_liquid_calibration/
-time ./simulator.exe 1 ../l7/mc_gas_calibration/
-time ./simulator.exe 1 ../l7/mc_solid/
-time ./simulator.exe 1 ../l7/mc_liquid/
-time ./simulator.exe 1 ../l7/mc_gas/
